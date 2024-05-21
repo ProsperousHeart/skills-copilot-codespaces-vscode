61bbdd1db894a094a226b616dbefd7d32ad97c7f
Kassandra Keeton <ProsperousHeart@users.noreply.github.com>
2024-05-21 03:45:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var member = {
+        name: "Aneesh",
+        age: 25,
+        skills: ["Javascript", "NodeJS", "ReactJS", "AngularJS"]
+    };
+    return member;
+}
